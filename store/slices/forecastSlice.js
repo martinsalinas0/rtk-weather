b5d23378048e934e6ee9e863a642017615c8f872
martinsalinas0 <martinlsalinas@pm.me>
2025-03-25 23:25:19 -0500

    fix: forecastSlice.js

--- store/slices/forecastSlice.js
+++ store/slices/forecastSlice.js
@@ -2,36 +2,64 @@ import axios from "axios";
 import {
   createAsyncThunk,
   createSlice,
-  isRejectedWithValue,
 } from "@reduxjs/toolkit";
 
 const API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
 const ROOT_URL = "https://api.openweathermap.org/data/2.5";
 
 export const fetchData = createAsyncThunk(
-  "weather/fecthData",
-  async (searchQuery, { isRejectedWithValue }) => {
+  "weather/fetchData", // Corrected action name
+  async (searchQuery, { rejectWithValue }) => {
     try {
       const response = await axios.get(
         `${ROOT_URL}/forecast?q=${searchQuery}&appid=${API_KEY}&units=imperial`
       );
       return response.data;
     } catch (error) {
-      return isRejectedWithValue(error.response || "error");
+      return rejectWithValue(error.response || "error");
     }
   }
 );
 
+export const forecastSlice = createSlice({
+  name: "cities",
+  initialState: {
+    
+      cities: [], 
+      status: 'idle', 
+      error: null, 
+    
 
-export const forecastSlice = createSlice({ 
-  name: 'cities', 
-  initialState: { 
-    cities: [], 
-     },
-  reducers: { 
+  },
+  reducers: {
     addCity: (state, action) => { 
-      state.cities = 
+      state.cities.push(action.payload)
+    },
+
+    deleteCity: (state, action) => { 
+      state.cities = state.cities.filter(
+        (city) => city.name !== action.payload
+      )
     }
-  }
-  
-})
+
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchForecast.pending, (state) => {
+        state.status = "loading...";
+      })
+      .addCase(fetchForecast.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.forecastData = action.payload;
+      })
+      .addCase(fetchForecast.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload; 
+      });
+  },
+});
+
+export const { resetForecast } = forecastSlice.actions;
+
+
+export default forecastSlice.reducer;

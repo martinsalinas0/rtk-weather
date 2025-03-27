2386e58e31a2a017687c3bd67bf7893cfa2e74b8
martinsalinas0 <martinlsalinas@pm.me>
2025-03-27 02:31:46 -0500

    update: forecastSlice.js

--- store/slices/forecastSlice.js
+++ store/slices/forecastSlice.js
@@ -1,67 +1,91 @@
-import axios from 'axios';
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from "axios";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
-const ROOT_URL = 'https://api.openweathermap.org/data/2.5';
+const ROOT_URL = "https://api.openweathermap.org/data/2.5";
+
+const initialState = {
+  cities: [],
+  status: "idle",
+  error: "",
+  forecastData: {},
+};
 
 export const fetchForecast = createAsyncThunk(
-  'weather/fetchForecast',
+  "forecast/fetchForecast",
   async (searchQuery, { rejectWithValue }) => {
     try {
       const response = await axios.get(
         `${ROOT_URL}/forecast?q=${searchQuery}&appid=${API_KEY}&units=imperial`
       );
-      return response.data;
+
+      const forecastData = response.data;
+
+      const weatherInfo = {
+        id: forecastData.city.id,
+        name: forecastData.city.name,
+        list: forecastData.list,
+        temperature: forecastData.list.map((item) => item.main.temp),
+        humidity: forecastData.list.map((item) => item.main.humidity),
+        pressure: forecastData.list.map((item) => item.main.pressure),
+      };
+
+      // console.log(forecastData);
+      // console.log(weatherInfo);
+      return weatherInfo;
     } catch (error) {
-      return rejectWithValue(error.response || 'error');
+      return rejectWithValue(error.response + 'ee' || "error");
     }
   }
 );
 
 const forecastSlice = createSlice({
-  name: 'weather',
-  initialState: {
-    cities: [
-      { 
-      name: 'Austin', 
-      tmep: 23, 
-      pressure: 1389, 
-      humidity: 32, 
-    }, 
-  ],
-    status: 'idle',
-    error: null,
-  },
+  name: "forecast",
+  initialState,
   reducers: {
     addCity: (state, action) => {
       state.cities.push(action.payload);
     },
     resetList: (state) => {
-      state.forecastData = null;
-      state.status = 'idle';
-      state.error = null;
+      state.cities = [];
+      state.status = "idle";
+      state.error = "";
+      state.forecastData = {};
     },
     deleteCity: (state, action) => {
-      state.cities = state.cities.filter((city) => city.name !== action.payload)
-    }
+      state.cities = state.cities.filter((city) => city.id !== action.payload);
+    },
   },
   extraReducers: (builder) => {
     builder
       .addCase(fetchForecast.pending, (state) => {
-        state.status = 'loading';
+        state.status = "loading";
       })
       .addCase(fetchForecast.fulfilled, (state, action) => {
-        state.status = 'succeeded';
+        state.status = "succeeded";
+        state.error = null; 
         state.forecastData = action.payload;
-        state.cities.push(action.payload.city.name);
+        // state.cities.push(action.payload);
+        console.log(action.payload);
+        const cityIdNo = state.cities.some(
+          (city) => city.id === action.payload.id
+        );
+        console.log(cityIdNo);
+
+        if (!cityIdNo) {
+          state.cities.push(action.payload);
+        } else {
+          console.log("duplicate");
+          alert("city already here");
+        }
       })
       .addCase(fetchForecast.rejected, (state, action) => {
-        state.status = 'failed';
+        state.status = "failed";
         state.error = action.error.message;
+
       });
   },
 });
 
 export const { addCity, resetList, deleteCity } = forecastSlice.actions;
-
 export default forecastSlice.reducer;

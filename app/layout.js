4b337fd9970d0966cdfd63611fd4245e2427255b
martinsalinas0 <martinlsalinas@pm.me>
2025-03-27 13:24:06 -0500

    updates

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,19 @@
-
-'use client'
+"use client";
 import "bootstrap/dist/css/bootstrap.css";
 import { Signika_Negative } from "next/font/google";
 import { Provider } from "react-redux";
+import store from "@/store/configureStore";
 
 const signikaNegative = Signika_Negative({
   subsets: ["latin"],
   weight: ["400", "500", "700"],
 });
 
-
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={signikaNegative.className}>
-        <Provider>{children}</Provider>
+        <Provider store={store}>{children}</Provider>
       </body>
     </html>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,11 @@
+"use client";
 import "bootstrap/dist/css/bootstrap.css";
-
-
+import CityView from "./components/cityView";
 
 export default function Home() {
   return (
     <main>
-       
+      <CityView />
     </main>
-  ); 
+  );
 }
